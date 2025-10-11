21fc49fc0e31c331c29a7f5f23c071676a2f02f8
ljashub <liasistculxd@gmail.com>
2025-10-12 00:36:19 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,3 @@
--- === TELEPORT-SAFE LOADER: Immer NUR dieses Script als Executor/autoload nutzen! ===
 local url = "https://raw.githubusercontent.com/ljashub/bloxstrike-beta/main/secretmecret.lua"
 if queue_on_teleport then
     queue_on_teleport("loadstring(game:HttpGet('" .. url .. "'))()")
